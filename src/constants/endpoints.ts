7eed56ebeaf4789dee958c408c77d604b65d6430
h.t <h@t.co>
2023-07-10 20:40:47 +0530

    added netlify functions

--- src/constants/endpoints.ts
+++ src/constants/endpoints.ts
@@ -1,6 +1,6 @@
 const BASE_URL = `/.netlify/functions`
 const PORT = '/'
 export default {
-  COUNTRIES: `${BASE_URL}${PORT}/countries`,
-  PRICE_OFFERS: `${BASE_URL}${PORT}/priceOffers`
+  COUNTRIES: `${BASE_URL}${PORT}countries`,
+  PRICE_OFFERS: `${BASE_URL}${PORT}priceOffers`
 }
